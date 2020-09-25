5217a0bd73c76f552ffa17803a62a33d9a6a4ed5
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-09-25 13:18:24 +0200

    Disable n-step replay

--- sb3_contrib/tqc/tqc.py
+++ sb3_contrib/tqc/tqc.py
@@ -3,7 +3,6 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
 import numpy as np
 import torch as th
 from stable_baselines3.common import logger
-from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.common.noise import ActionNoise
 from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
@@ -75,8 +74,6 @@ class TQC(OffPolicyAlgorithm):
         n_episodes_rollout: int = -1,
         action_noise: Optional[ActionNoise] = None,
         optimize_memory_usage: bool = False,
-        replay_buffer_class: Optional[Type[ReplayBuffer]] = None,
-        replay_buffer_kwargs: Optional[Dict[str, Any]] = None,
         ent_coef: Union[str, float] = "auto",
         target_update_interval: int = 1,
         target_entropy: Union[str, float] = "auto",
@@ -98,8 +95,6 @@ class TQC(OffPolicyAlgorithm):
             env,
             TQCPolicy,
             learning_rate,
-            replay_buffer_class,
-            replay_buffer_kwargs,
             buffer_size,
             learning_starts,
             batch_size,
