5d7b79d41af132497e8c3271db8a9933034af9f8
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-10-12 20:17:33 +0200

    Improve coverage

--- tests/test_save_load.py
+++ tests/test_save_load.py
@@ -177,17 +177,17 @@ def test_set_env(model_class):
     # create model
     model = model_class("MlpPolicy", env, policy_kwargs=dict(net_arch=[16]))
     # learn
-    model.learn(total_timesteps=500)
+    model.learn(total_timesteps=300)
 
     # change env
     model.set_env(env2)
     # learn again
-    model.learn(total_timesteps=500)
+    model.learn(total_timesteps=300)
 
     # change env test wrapping
     model.set_env(env3)
     # learn again
-    model.learn(total_timesteps=500)
+    model.learn(total_timesteps=300)
 
 
 @pytest.mark.parametrize("model_class", MODEL_LIST)
