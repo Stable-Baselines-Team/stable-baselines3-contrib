e8fb6901176095d62a3807d668fe725a2c0fa715
Collin Stedman <kronion@users.noreply.github.com>
2021-03-27 16:22:04 -0500

    Add wrappers

---
+++ sb3_contrib/common/wrappers/action_masker.py
@@ -0,0 +1,33 @@
+from typing import Callable, Union
+
+import gym
+import numpy as np
+
+
+class ActionMasker(gym.Wrapper):
+    """
+    Vectorized environment base class
+
+    :param env: the Gym environment to wrap
+    :param action_mask_fn: A function that takes a Gym environment and returns an action mask,
+        or the name of such a method provided by the environment.
+    """
+
+    def __init__(
+        self, env: gym.Env, action_mask_fn: Union[str, Callable[[gym.Env], np.ndarray]]
+    ):
+        super().__init__(env)
+
+        if isinstance(action_mask_fn, str):
+            found_method = getattr(self.env, action_mask_fn)
+            if not callable(found_method):
+                raise ValueError(
+                    f"Environment attribute {action_mask_fn} is not a method"
+                )
+
+            self.action_action_mask_fn = found_method
+        else:
+            self.action_mask_fn = action_mask_fn
+
+    def valid_actions(self) -> np.ndarray:
+        return self.action_mask_fn(self.env)
