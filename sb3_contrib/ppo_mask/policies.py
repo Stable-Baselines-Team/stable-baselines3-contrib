349c97d43732307aafbb466aee481d01e58258fc
Collin Stedman <kronion@users.noreply.github.com>
2021-05-11 00:44:19 -0500

    Check codestyle

--- sb3_contrib/ppo_mask/policies.py
+++ sb3_contrib/ppo_mask/policies.py
@@ -2,7 +2,6 @@ from stable_baselines3.common.policies import register_policy
 
 from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
 
-
 MlpPolicy = MaskableActorCriticPolicy
 
 register_policy("MlpPolicy", MlpPolicy)
