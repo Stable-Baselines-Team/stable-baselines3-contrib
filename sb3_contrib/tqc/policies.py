1306c3c64eb12613464982c66cb416a3bbc66285
Antonin Raffin <antonin.raffin@ensta.org>
2021-06-02 12:19:22 +0200

    Add layer nprm for all policies

--- sb3_contrib/tqc/policies.py
+++ sb3_contrib/tqc/policies.py
@@ -464,6 +464,7 @@ class CnnPolicy(TQCPolicy):
     :param n_critics: Number of critic networks to create.
     :param share_features_extractor: Whether to share or not the features extractor
         between the actor and the critic (this saves computation time)
+    :param layer_norm: Whether to use layer normalization for the actor or not.
     """
 
     def __init__(
@@ -486,6 +487,7 @@ class CnnPolicy(TQCPolicy):
         n_quantiles: int = 25,
         n_critics: int = 2,
         share_features_extractor: bool = True,
+        layer_norm: bool = False,
     ):
         super(CnnPolicy, self).__init__(
             observation_space,
@@ -506,6 +508,7 @@ class CnnPolicy(TQCPolicy):
             n_quantiles,
             n_critics,
             share_features_extractor,
+            layer_norm,
         )
 
 
@@ -538,6 +541,7 @@ class MultiInputPolicy(TQCPolicy):
     :param n_critics: Number of critic networks to create.
     :param share_features_extractor: Whether to share or not the features extractor
         between the actor and the critic (this saves computation time)
+    :param layer_norm: Whether to use layer normalization for the actor or not.
     """
 
     def __init__(
@@ -560,6 +564,7 @@ class MultiInputPolicy(TQCPolicy):
         n_quantiles: int = 25,
         n_critics: int = 2,
         share_features_extractor: bool = True,
+        layer_norm: bool = False,
     ):
         super(MultiInputPolicy, self).__init__(
             observation_space,
@@ -580,6 +585,7 @@ class MultiInputPolicy(TQCPolicy):
             n_quantiles,
             n_critics,
             share_features_extractor,
+            layer_norm,
         )
 
 
