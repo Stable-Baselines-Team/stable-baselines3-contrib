d749cca2073de48b0bc3759a4e5f82a114d9baf5
sgillen <sgillen@ucsb.edu>
2021-09-21 10:15:37 -0700

    rollback so there are now predict workers, some refactoring

--- sb3_contrib/ars/ars.py
+++ sb3_contrib/ars/ars.py
@@ -13,6 +13,8 @@ from stable_baselines3.common.policies import BasePolicy
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import get_schedule_fn, safe_mean
 
+from torch.multiprocessing import Queue, Process
+
 
 class ARS(BaseAlgorithm):
     """
@@ -40,8 +42,8 @@ class ARS(BaseAlgorithm):
             env:  Union[GymEnv, str],
             n_delta: int = 64,
             n_top: Optional[int] = None,
-            alpha: Union[float, Schedule] = 0.02,
-            sigma: Union[float, Schedule] = 0.025,
+            alpha: Union[float, Schedule] = 0.05,
+            sigma: Union[float, Schedule] = 0.05,
             policy_kwargs: Optional[Dict[str, Any]] = None,
             policy_base: Type[BasePolicy] = ARSPolicy,
             tensorboard_log: Optional[str] = None,
@@ -105,59 +107,61 @@ class ARS(BaseAlgorithm):
         self.n_params = len(self.theta)
         self.policy = self.policy.to(self.device)
 
-    def _collect_rollouts(self, deltas, callback):
-        batch_steps = 0
-        theta_idx = 0
+    def _collect_rollouts(self, policy_deltas):
+        with th.no_grad():
+            batch_steps = 0
+            theta_idx = 0
 
-        # Generate 2*n_delta candidate policies by adding noise to the current theta
-        candidate_thetas = np.concatenate([self.theta + deltas, self.theta - deltas])
-        candidate_returns = np.zeros(candidate_thetas.shape[0]) # returns == sum of rewards
-        self.ep_info_buffer = []
+            # Generate 2*n_delta candidate policies by adding noise to the current theta
+            candidate_thetas = np.concatenate([self.theta + policy_deltas, self.theta - policy_deltas])
+            candidate_returns = np.zeros(candidate_thetas.shape[0]) # returns == sum of rewards
+            self.ep_info_buffer = []
 
-        callback.on_rollout_start()
-        while theta_idx < candidate_returns.shape[0]:
-            policies = []
+            self.callback.on_rollout_start()
+            while theta_idx < candidate_returns.shape[0]:
+                policy_list = []
 
-            # We are using a vecenv with n_envs==n_workers. We batch our candidate theta evaluations into vectors
-            # of length n_workers
-            for _ in range(self.n_workers):
-                theta_tensor = th.tensor(candidate_thetas[theta_idx], requires_grad=False, dtype=self.dtype)
-                th.nn.utils.vector_to_parameters(theta_tensor, self.policy.parameters())
-                policies.append(copy.deepcopy(self.policy))
-                theta_idx += 1
+                # We are using a vecenv with n_envs==n_workers. We batch our candidate theta evaluations into vectors
+                # of length n_workers
+                for _ in range(self.n_workers):
+                    theta_tensor = th.tensor(candidate_thetas[theta_idx], dtype=self.dtype)
+                    th.nn.utils.vector_to_parameters(theta_tensor, self.policy.parameters())
+                    policy_list.append(copy.deepcopy(self.policy))
+                    theta_idx += 1
 
-            # We only want one rollout per policy, but vec envs will automatically reset when a done is sent.
-            # To get around this we use vec_done_once to keep track of which environments have already finished,
-            # and we ignore rewards once we see the first done.
-            vec_obs = self.env.reset()
-            vec_acts = np.zeros((self.n_workers, self.action_space.shape[0]))
-            vec_returns = np.zeros(self.n_workers)
-            vec_done_once = np.zeros(self.n_workers, dtype=bool)  # Tracks if each env has returned done at least once
-            vec_steps_taken = np.zeros(self.n_workers) # used to update logs
+                # We only want one rollout per policy, but vec envs will automatically reset when a done is sent.
+                # To get around this we use vec_done_once to keep track of which environments have already finished,
+                # and we ignore rewards once we see the first done.
+                vec_obs = self.env.reset()
+                vec_acts = np.zeros((self.n_workers, self.action_space.shape[0]))
+                vec_returns = np.zeros(self.n_workers)
+                vec_done_once = np.zeros(self.n_workers, dtype=bool)  # Tracks if each env has returned done at least once
+                vec_steps_taken = np.zeros(self.n_workers) # used to update logs
 
-            while not np.all(vec_done_once):
-                for i in range(self.n_workers):
-                    vec_acts[i, :] = policies[i].predict(vec_obs[i, :])[0]
-                vec_obs, vec_rews, vec_done, vec_info = self.env.step(vec_acts)
-                callback.on_step()
-                vec_rews += self.alive_bonus_offset # Alive offset from the original paper, defaults to zero
+                while not np.all(vec_done_once):
+                    for i in range(self.n_workers):
+                        vec_acts[i, :] = policy_list[i].predict(vec_obs[i, :])[0]
+                    vec_obs, vec_rews, vec_done, vec_info = self.env.step(vec_acts)
+                    self.callback.on_step()
 
-                # The order of episodes in the ep_info_buffer won't match up with the order of theta, which feels gross,
-                # But we are only using ep_info for logging so I don't think that should natter
-                for i, (done_this_time, done_before) in enumerate(zip(vec_done, vec_done_once)):
-                    if done_this_time and not done_before:
-                        maybe_ep_info = vec_info[i].get('episode')
-                        if maybe_ep_info is not None:
-                            self.ep_info_buffer.append(vec_info[i]['episode'])
+                    vec_rews += self.alive_bonus_offset # Alive offset from the original paper, defaults to zero
 
-                vec_steps_taken += np.invert(vec_done_once) # Count the step even if we received a done
-                vec_done_once = np.bitwise_or(vec_done, vec_done_once)
-                vec_returns += vec_rews*np.invert(vec_done_once)  # Don't count rewards if we have reset
+                    # The order of episodes in the ep_info_buffer won't match up with the order of theta, which feels gross,
+                    # But we are only using ep_info for logging so I don't think that should natter
+                    for i, (done_this_time, done_before) in enumerate(zip(vec_done, vec_done_once)):
+                        if done_this_time and not done_before:
+                            maybe_ep_info = vec_info[i].get('episode')
+                            if maybe_ep_info is not None:
+                                self.ep_info_buffer.append(maybe_ep_info)
 
-            candidate_returns[theta_idx-self.n_workers:theta_idx] = vec_returns
-            batch_steps += sum(vec_steps_taken)  # only count steps used in the update step towards our total_steps
+                    vec_steps_taken += np.invert(vec_done_once)
+                    vec_returns += vec_rews*np.invert(vec_done_once)
+                    vec_done_once = np.bitwise_or(vec_done, vec_done_once)
 
-        callback.on_rollout_end()
+                candidate_returns[theta_idx-self.n_workers:theta_idx] = vec_returns
+                batch_steps += sum(vec_steps_taken)  # only count steps used in the update step towards our total_steps
+
+            self.callback.on_rollout_end()
 
         return candidate_returns, batch_steps
 
@@ -191,6 +195,33 @@ class ARS(BaseAlgorithm):
         self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
         self.logger.dump(step=self.num_timesteps)
 
+    def _do_one_update(self):
+        sigma_this_step = self.sigma(self._current_progress_remaining)
+        alpha_this_step = self.alpha(self._current_progress_remaining)
+
+        deltas = self.rng.standard_normal((self.n_delta, self.n_params))
+
+        candidate_returns, batch_steps = self._collect_rollouts(deltas * sigma_this_step)
+
+        plus_returns = candidate_returns[:self.n_delta]
+        minus_returns = candidate_returns[self.n_delta:]
+
+        top_returns = np.zeros_like(plus_returns)
+        for i in range(len(top_returns)):
+            top_returns[i] = np.max([plus_returns[i], minus_returns[i]])
+
+        top_idx = np.argsort(top_returns)[::-1][:self.n_top]
+        plus_returns = plus_returns[top_idx]
+        minus_returns = minus_returns[top_idx]
+        deltas = deltas[top_idx]
+
+        update_return_std = np.concatenate([plus_returns, minus_returns]).std()
+        step_size = alpha_this_step / (self.n_top * update_return_std + 1e-6)
+        self.theta = self.theta + step_size * (plus_returns - minus_returns) @ deltas
+
+        self._n_updates += 1
+        self.num_timesteps += batch_steps
+
     def learn(
             self,
             total_timesteps: int,
@@ -209,51 +240,32 @@ class ARS(BaseAlgorithm):
             total_steps, eval_env, callback, eval_freq, n_eval_episodes, eval_log_path, reset_num_timesteps, tb_log_name
         )
 
+        self.callback = callback
+
         self.n_workers = self.env.num_envs # TODO can I do this somewhere earlier?? when is the env loaded ... ?
         self._validate_hypers()
-
-        callback.on_training_start(locals(), globals())
+        self.callback.on_training_start(locals(), globals())
 
         while self.num_timesteps < total_steps:
             self._update_current_progress_remaining(self.num_timesteps, total_timesteps)
-            sigma_this_step = self.sigma(self._current_progress_remaining)
-            alpha_this_step = self.alpha(self._current_progress_remaining)
 
-            deltas = self.rng.standard_normal((self.n_delta, self.n_params)) * sigma_this_step
+            self._do_one_update()
 
-            candidate_returns, batch_steps = self._collect_rollouts(deltas, callback)
-
-            plus_returns = candidate_returns[:self.n_delta]
-            minus_returns = candidate_returns[self.n_delta:]
-
-            top_returns = np.zeros_like(plus_returns)
-            for i, ret in enumerate(zip(plus_returns, minus_returns)):
-                top_returns[i] = np.max([plus_returns[i], minus_returns[i]])
-
-            top_idx = np.argsort(top_returns)[::-1][:self.n_top]
-            plus_returns = plus_returns[top_idx]
-            minus_returns = minus_returns[top_idx]
-
-            update_return_std = np.concatenate([plus_returns, minus_returns]).std()
-            step_size = alpha_this_step / (self.n_top * update_return_std + 1e-6)
-            self.theta = self.theta + step_size*np.sum((plus_returns - minus_returns)*deltas[top_idx].T, axis=1)
-
-            self._n_updates += 1
-            self.num_timesteps += batch_steps
             if log_interval is not None and self._n_updates % log_interval == 0:
                 self._log_and_dump()
 
-        theta_tensor = th.tensor(self.theta, requires_grad=False, dtype=self.dtype, device=self.device)
+        theta_tensor = th.tensor(self.theta, dtype=self.dtype, device=self.device) # leave requires_grad how we found it
         torch.nn.utils.vector_to_parameters(theta_tensor, self.policy.parameters())
 
-        callback.on_training_end()
+        self.callback.on_training_end()
+
         return self
 
 
 if __name__ == "__main__":
     # Some more args
 
-    from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
+    from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
     from stable_baselines3.common.env_util import make_vec_env
 
     env_name = "Swimmer-v2"
@@ -261,9 +273,9 @@ if __name__ == "__main__":
     venv = make_vec_env(env_name, 16, vec_env_cls=SubprocVecEnv)
     venv = VecNormalize(venv, norm_reward=False)
 
-    model = ARS("LinearPolicy", venv, verbose=1, n_delta=32, alpha=0.02, sigma=0.03)
+    model = ARS("LinearPolicy", venv, verbose=2, n_delta=32, alpha=0.02, sigma=0.03)
 
-    model.learn(5e6, log_interval=10)
+    model.learn(5e6, log_interval=1)
     from stable_baselines3.common.evaluation import evaluate_policy
 
     evaluate_policy(model, venv)
