9ae77683a5b58e192dfaf8f7bef54bc17ad1b304
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-02-23 17:23:54 +0100

    typo mainly

--- sb3_contrib/diayn/diayn.py
+++ sb3_contrib/diayn/diayn.py
@@ -1,4 +1,3 @@
-import copy
 from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
 import gym
@@ -14,45 +13,6 @@ from stable_baselines3.common.utils import get_device, obs_as_tensor
 import torch.nn.functional as F
 from torch import nn
 from torch.distributions import Categorical
-
-
-class FeatureExtractor(nn.Module):
-    """
-    The feature extractor takes as input an observation and returns its feature representation.
-
-    :param obs_dim: Observation dimension
-    :param net_arch: The specification of the network
-    :param feature_dim: The feature dimension
-    :param activation_fn: The activation function to use for the networks
-    :param device:
-    """
-
-    def __init__(
-        self,
-        obs_dim: int,
-        net_arch: List[int],
-        feature_dim: int,
-        activation_fn: Type[nn.Module] = nn.ReLU,
-        device: Union[th.device, str] = "auto",
-    ) -> None:
-        super().__init__()
-        self.device = get_device(device)
-        self.feature_dim = feature_dim
-        layers = []
-        last_layer_dim = obs_dim
-        # Iterate through the shared layers and build the shared parts of the network
-        for layer_dim in net_arch:
-            layers.append(nn.Linear(last_layer_dim, layer_dim))  # add linear of size layer
-            layers.append(activation_fn())
-            last_layer_dim = layer_dim
-        layers.append(nn.Linear(last_layer_dim, feature_dim))
-
-        # Create networks
-        self.net = nn.Sequential(*layers).to(self.device)
-
-    def forward(self, obs: th.Tensor) -> th.Tensor:
-        obs_feature = self.net(obs)
-        return obs_feature
 
 
 class Discriminator(nn.Module):
@@ -97,17 +57,16 @@ class Discriminator(nn.Module):
         return self.net(obs)
 
 
-class Goalify(gym.GoalEnv, gym.Wrapper):
+class SkillWrapper(gym.GoalEnv, gym.Wrapper):
     """
-    Make an envrionment goal-conditionned.
+    Make an envrionment skill-conditionned.
 
-    The goal is to reach a specific latent goal.
-    ``reward`` is :math:`r = \log q_\varphi(z | s_{t+1}) - log p(z)` where :math:`q_\varphi` is
-    the discriminator, :math:`z` the latent representation, and :math:`p` the skill distribution.
+    The observation return by step is a dict composed of the ``observation`` and the ``skill``.
+    ``reward`` is :math:`r = \log q(z | s_{t+1}) - log p(z)` where :math:`q` is the discriminator,
+    :math:`z` the skill, and :math:`p` the categorical skill distribution.
 
     :param env: The environment
     :param skill_distribution: The categorical skill distribution
-    :param skills: The skills (from latent space)
     :param discriminator: The discrminator, mapping obs to skill
     """
 
@@ -122,33 +81,31 @@ class Goalify(gym.GoalEnv, gym.Wrapper):
         self.skill_distribution = skill_distribution
         self.nb_skills = self.skill_distribution.param_shape[0]
         self.discriminator = discriminator
+        observation_space = self.env.observation_space
         self.observation_space = spaces.Dict(
             {
-                "observation": copy.deepcopy(self.env.observation_space),
-                "desired_goal": spaces.Box(-10, 10, (self.nb_skills,)),
+                "observation": spaces.Box(observation_space.low, observation_space.high, observation_space.shape),
+                "skill": spaces.Box(-10, 10, (self.nb_skills,)),
             }
         )
 
-    @property
-    def spec(self):
-        return self.env.spec
-
     def step(self, action: np.ndarray) -> Tuple[Dict[str, np.ndarray], float, bool, Dict[str, Any]]:
         obs, reward, done, info = self.env.step(action)
-        obs = obs.astype(np.float32)
+        obs = obs.astype(np.float32)  # ensure that observation type is float32
         reward = self._compute_reward(obs)
         return self._get_dict_obs(obs), reward, done, info
 
     def reset(self) -> Dict[str, np.ndarray]:
-        self.skill = self.skill_distribution.sample()
-        obs = self.env.reset().astype(np.float32)
+        self.skill = self.skill_distribution.sample()  # sample a skill
+        obs = self.env.reset().astype(np.float32)  # reset the env and ensure that observation type is float32
         return self._get_dict_obs(obs)
 
     def _get_dict_obs(self, obs: np.ndarray) -> Dict[str, np.ndarray]:
-        desired_goal = F.one_hot(self.skill, num_classes=self.nb_skills)
+        # Returns the dict observation composed of the wrapped observation and the skill.
+        skill = F.one_hot(self.skill, num_classes=self.nb_skills)
         return {
             "observation": obs,
-            "desired_goal": desired_goal,
+            "skill": skill,
         }
 
     def _compute_reward(self, obs: np.ndarray) -> Union[float, np.ndarray]:
@@ -156,11 +113,11 @@ class Goalify(gym.GoalEnv, gym.Wrapper):
         Returns the reward as :math:`r = \log q_\varphi(z | s_{t+1}) - log p(z)` where :math:`q_\varphi` is
         the discriminator, :math:`z` the latent representation, and :math:`p` the skill distribution.
 
-        :param obs: Achieved goal
+        :param obs: Curent observation
         :return: Reward or array of rewards, depending on the input
         """
         obs_th = obs_as_tensor(obs, self.discriminator.device)
-        probs = self.discriminator.probs(obs_th)
+        probs = self.discriminator.probs(obs_th)  # q_\varphi(⸱ | s_{t+1})
         prob = probs[self.skill]  # q_\varphi(z | s_{t+1})
         skill_log_prob = self.skill_distribution.log_prob(self.skill)  # log p(z)
         reward = th.log(prob) - skill_log_prob
@@ -172,6 +129,7 @@ class DIAYN(SAC):
         self,
         env: Union[GymEnv, str],
         nb_skills: int,
+        net_arch: Optional[List] = None,
         learning_rate: Union[float, Schedule] = 0.0003,
         buffer_size: int = 1000000,
         learning_starts: int = 100,
@@ -201,9 +159,11 @@ class DIAYN(SAC):
     ):
 
         obs_dim = env.observation_space.shape[0]
+        if net_arch is None:
+            net_arch = [256, 256]
         skill_distribution = Categorical(logits=th.ones(nb_skills))
-        self.discriminator = Discriminator(obs_dim, net_arch=[256, 256], nb_skills=nb_skills)
-        env = Goalify(env, skill_distribution, self.discriminator)
+        self.discriminator = Discriminator(obs_dim, net_arch=net_arch, nb_skills=nb_skills)
+        env = SkillWrapper(env, skill_distribution, self.discriminator)
         self.discriminator_optimizer = th.optim.SGD(self.discriminator.parameters(), learning_rate)
 
         super().__init__(
@@ -241,13 +201,12 @@ class DIAYN(SAC):
         super().train(gradient_steps, batch_size)
 
         discriminator_losses = []
-
         for gradient_step in range(gradient_steps):
             # Sample replay buffer
             replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)
-            observations = replay_data.observations["observation"]
+            observations = replay_data.next_observations["observation"]
             input = self.discriminator.probs(observations)
-            target = replay_data.observations["desired_goal"]
+            target = replay_data.observations["skill"]
             discriminator_loss = F.cross_entropy(input, target)
 
             # Optimize the discriminator
