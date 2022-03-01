d1e43711ab1cff9946ffa7f89e0cd9155e0007ab
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-03-01 10:48:45 +0100

    use vectorized her

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     packages=[package for package in find_packages() if package.startswith("sb3_contrib")],
     package_data={"sb3_contrib": ["py.typed", "version.txt"]},
     install_requires=[
-        "stable_baselines3 @ git+https://git@github.com/qgallouedec/stable-baselines3@IM_compat",
+        "stable_baselines3 @ git+https://git@github.com/qgallouedec/stable-baselines3@IM_compat_and_her_alt",
     ],
     description="Contrib package of Stable Baselines3, experimental code.",
     author="Antonin Raffin",
