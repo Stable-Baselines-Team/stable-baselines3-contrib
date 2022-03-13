dc610676809920e791fc88a97984ed3e15a709dd
Antonin Raffin <antonin.raffin@ensta.org>
2022-03-13 15:11:16 +0100

    Update min pytorch version

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
     package_data={"sb3_contrib": ["py.typed", "version.txt"]},
     install_requires=[
         "stable_baselines3>=1.4.1a1",
+        "torch>=1.11",
     ],
     description="Contrib package of Stable Baselines3, experimental code.",
     author="Antonin Raffin",
