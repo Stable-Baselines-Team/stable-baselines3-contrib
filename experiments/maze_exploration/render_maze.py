6d33d90a62576cb015e15f0f60a19f72d2cd9a3f
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-03-19 09:49:55 +0100

    render with grid

--- experiments/maze_exploration/render_maze.py
+++ experiments/maze_exploration/render_maze.py
@@ -12,6 +12,7 @@ OFFSET = SCREEN_DIM // 2
 
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
+GRAY = pygame.Color(192, 192, 192)
 GREEN = pygame.Color(0, 200, 0)
 RED = pygame.Color(255, 0, 0)
 BLUE = pygame.Color(0, 0, 255)
@@ -20,6 +21,7 @@ BLUE = pygame.Color(0, 0, 255)
 def draw_filled_circle(surf, pos, radius=1, color=RED):
     x, y = pos * SCALE + OFFSET
     gfxdraw.filled_circle(surf, int(x), int(y), radius, color)
+
 
 def draw_circle(surf, pos, radius=3, color=GREEN):
     x, y = pos * SCALE + OFFSET
@@ -32,13 +34,19 @@ def draw_line(surf, point_a, point_b, color=BLACK):
     gfxdraw.line(surf, int(x1), int(y1), int(x2), int(y2), color)
 
 
-def render_and_save(envs: List, filename: str, goals=None, bg=WHITE):
+def render_and_save(envs: List, filename: str, goals=None, bg=WHITE, grid=None):
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_DIM, SCREEN_DIM))
     surf = pygame.Surface((SCREEN_DIM, SCREEN_DIM))
 
     # Background
     surf.fill(bg)
+
+    if grid is not None:
+        origin, width = grid
+        for i in range(200):
+            draw_line(surf, np.array([-13, (i - 100 + origin) * width]), np.array([13, (i - 100 + origin) * width]), GRAY)
+            draw_line(surf, np.array([(i - 100 + origin) * width, -13]), np.array([(i - 100 + origin) * width, 13]), GRAY)
 
     # Draw visited points
     all_pos = np.concatenate([env.all_pos for env in envs])
