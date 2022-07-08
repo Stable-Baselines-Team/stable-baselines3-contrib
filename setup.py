b13b51c87ada5e74a7d6878bf6788d619d4bac60
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-07-08 16:46:02 +0200

    update dependency

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     packages=[package for package in find_packages() if package.startswith("sb3_contrib")],
     package_data={"sb3_contrib": ["py.typed", "version.txt"]},
     install_requires=[
-        "stable_baselines3 @ git+https://git@github.com/qgallouedec/stable-baselines3@IM_compat_and_her_alt",
+        "stable_baselines3 @ git+https://git@github.com/qgallouedec/stable-baselines3@IM_and_Vec_HER",
     ],
     description="Contrib package of Stable Baselines3, experimental code.",
     author="Antonin Raffin",
