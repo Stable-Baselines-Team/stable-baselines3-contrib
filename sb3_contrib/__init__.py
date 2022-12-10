823760b5d0e82db56f4ef1dd121b08a5e8344bf0
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-10 11:51:55 +0100

    typo

--- sb3_contrib/__init__.py
+++ sb3_contrib/__init__.py
@@ -15,8 +15,7 @@ with open(version_file) as file_handler:
 
 __all__ = [
     "ARS",
-    "DuelingDQN"
-    "MaskablePPO",
+    "DuelingDQN" "MaskablePPO",
     "RecurrentPPO",
     "QRDQN",
     "TQC",
