365bdb90d63ab436da696beb5a5f0cb786453c4d
AlexPasqua <alexpasquali98@gmail.com>
2025-10-26 18:13:46 +0100

    Created HybridToBoxWrapper to handle hybrid actions but still integrate with the library
    PPO and the base algorithm do some validation on the action space. If we want our HybridPPO to be subclass of PPO, we need this wrapper for the integration with the library.
    
    Co-authored-by: simrey <simonereynoso@gmail.com>

--- sb3_contrib/common/wrappers/__init__.py
+++ sb3_contrib/common/wrappers/__init__.py
@@ -1,4 +1,5 @@
 from sb3_contrib.common.wrappers.action_masker import ActionMasker
 from sb3_contrib.common.wrappers.time_feature import TimeFeatureWrapper
+from sb3_contrib.common.wrappers.hybrid_to_box_wrapper import HybridToBoxWrapper
 
-__all__ = ["ActionMasker", "TimeFeatureWrapper"]
+__all__ = ["ActionMasker", "TimeFeatureWrapper", "HybridToBoxWrapper"]
