b4191428e0bc2e4f83ce88ecdf00a36bb2dec5c8
AlexPasqua <alexpasquali98@gmail.com>
2025-11-05 10:20:41 +0100

    Revert "Created HybridToBoxWrapper to handle hybrid actions but still integrate with the library"
    This reverts commit 365bdb90d63ab436da696beb5a5f0cb786453c4d.

--- sb3_contrib/common/wrappers/__init__.py
+++ sb3_contrib/common/wrappers/__init__.py
@@ -1,5 +1,4 @@
 from sb3_contrib.common.wrappers.action_masker import ActionMasker
 from sb3_contrib.common.wrappers.time_feature import TimeFeatureWrapper
-from sb3_contrib.common.wrappers.hybrid_to_box_wrapper import HybridToBoxWrapper
 
-__all__ = ["ActionMasker", "TimeFeatureWrapper", "HybridToBoxWrapper"]
+__all__ = ["ActionMasker", "TimeFeatureWrapper"]
