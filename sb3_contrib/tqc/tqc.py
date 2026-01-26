0b08bb8b0bfedad3b5b68e5dfb7e910a85595f92
Antonin RAFFIN <antonin.raffin@ensta.org>
2026-01-26 08:58:44 +0100

    WIP uncertainty

--- sb3_contrib/tqc/tqc.py
+++ sb3_contrib/tqc/tqc.py
@@ -5,11 +5,13 @@ import numpy as np
 import torch as th
 from gymnasium import spaces
 from stable_baselines3.common.buffers import ReplayBuffer
+from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.noise import ActionNoise
 from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.policies import BasePolicy
-from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
-from stable_baselines3.common.utils import get_parameters_by_name, polyak_update
+from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, RolloutReturn, TrainFreq, TrainFrequencyUnit
+from stable_baselines3.common.utils import get_parameters_by_name, polyak_update, should_collect_more_steps
+from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv
 
 from sb3_contrib.common.utils import quantile_huber_loss
 from sb3_contrib.tqc.policies import Actor, CnnPolicy, Critic, MlpPolicy, MultiInputPolicy, TQCPolicy
@@ -326,3 +328,139 @@ class TQC(OffPolicyAlgorithm):
         else:
             saved_pytorch_variables = ["ent_coef_tensor"]
         return state_dicts, saved_pytorch_variables
+
+    def collect_rollouts(
+        self,
+        env: VecEnv,
+        callback: BaseCallback,
+        train_freq: TrainFreq,
+        replay_buffer: ReplayBuffer,
+        action_noise: ActionNoise | None = None,
+        learning_starts: int = 0,
+        log_interval: int | None = None,
+    ) -> RolloutReturn:
+        """
+        Collect experiences and store them into a ``ReplayBuffer``.
+
+        :param env: The training environment
+        :param callback: Callback that will be called at each step
+            (and at the beginning and end of the rollout)
+        :param train_freq: How much experience to collect
+            by doing rollouts of current policy.
+            Either ``TrainFreq(<n>, TrainFrequencyUnit.STEP)``
+            or ``TrainFreq(<n>, TrainFrequencyUnit.EPISODE)``
+            with ``<n>`` being an integer greater than 0.
+        :param action_noise: Action noise that will be used for exploration
+            Required for deterministic policy (e.g. TD3). This can also be used
+            in addition to the stochastic policy for SAC.
+        :param learning_starts: Number of steps before learning for the warm-up phase.
+        :param replay_buffer:
+        :param log_interval: Log data every ``log_interval`` episodes
+        :return:
+        """
+        # Switch to eval mode (this affects batch norm / dropout)
+        self.policy.set_training_mode(False)
+
+        num_collected_steps, num_collected_episodes = 0, 0
+
+        assert isinstance(env, VecEnv), "You must pass a VecEnv"
+        assert train_freq.frequency > 0, "Should at least collect one step or episode."
+
+        if env.num_envs > 1:
+            assert train_freq.unit == TrainFrequencyUnit.STEP, "You must use only one env when doing episodic training."
+
+        if self.use_sde:
+            self.actor.reset_noise(env.num_envs)  # type: ignore[operator]
+
+        callback.on_rollout_start()
+        continue_training = True
+        while should_collect_more_steps(train_freq, num_collected_steps, num_collected_episodes):
+            if self.use_sde and self.sde_sample_freq > 0 and num_collected_steps % self.sde_sample_freq == 0:
+                # Sample a new noise matrix
+                self.actor.reset_noise(env.num_envs)  # type: ignore[operator]
+
+            # Select action randomly or according to policy
+            actions, buffer_actions = self._sample_action(learning_starts, action_noise, env.num_envs)
+
+            # Compute uncertainty
+            # TODO: use proper obs
+            # Aleatoric
+            # n_envs x n_critics x n_quantiles
+            no_uncertainty_env_indices = []
+            if self.num_timesteps > learning_starts and True:
+                # Note: we assume n_envs = 1 for now
+                # TODO: handle for n_envs
+                aleatoric_threshold = 0.005
+                epistemic_threshold = 0.005
+                with th.no_grad():
+                    quantiles = self.critic(
+                        self.replay_buffer.to_torch(self._last_obs, copy=False),
+                        self.replay_buffer.to_torch(actions, copy=False),
+                    )
+                    # shape: (n_envs, n_critics)
+                    aleatoric_uncertainty = th.var(quantiles, dim=2).max().item()
+                    # shape: (n_envs, n_critics)
+                    critics_mean = th.mean(quantiles, dim=2)
+                    # shape: (n_envs, 1)
+                    # or: torch.diff(th_mean, dim=1).squeeze(dim=1)
+                    mean_diff = th.abs(critics_mean[:, 0] - critics_mean[:, 1]).max().item()
+                    if mean_diff < epistemic_threshold and aleatoric_uncertainty < aleatoric_threshold:
+                        no_uncertainty_env_indices = [0]
+                    if self.num_timesteps % 500 == 0 or no_uncertainty_env_indices:
+                        print(f"{self.num_timesteps=} {mean_diff=:.2f} {aleatoric_uncertainty=:.2f}")
+
+            # Rescale and perform action
+            new_obs, rewards, dones, infos = env.step(actions)
+
+            self.num_timesteps += env.num_envs
+            num_collected_steps += 1
+
+            # If too certain, truncate
+            for env_idx in no_uncertainty_env_indices:
+                if not dones[env_idx]:
+                    assert isinstance(self.env.unwrapped, DummyVecEnv)
+                    vec_env = self.env.unwrapped
+                    dones[env_idx] = True
+                    infos[env_idx]["TimeLimit.truncated"] = True
+                    infos[env_idx]["terminal_observation"] = new_obs[env_idx].copy()
+                    new_obs_single_env, _ = vec_env.envs[env_idx].reset()
+                    # TODO: update monitor to have the right stats
+                    vec_env._save_obs(env_idx, new_obs_single_env)
+                    new_obs[env_idx] = new_obs_single_env
+
+            # Give access to local variables
+            callback.update_locals(locals())
+            # Only stop training if return value is False, not when it is None.
+            if not callback.on_step():
+                return RolloutReturn(num_collected_steps * env.num_envs, num_collected_episodes, continue_training=False)
+
+            # Retrieve reward and episode length if using Monitor wrapper
+            self._update_info_buffer(infos, dones)
+
+            # Store data in replay buffer (normalized action and unnormalized observation)
+            self._store_transition(replay_buffer, buffer_actions, new_obs, rewards, dones, infos)  # type: ignore[arg-type]
+
+            self._update_current_progress_remaining(self.num_timesteps, self._total_timesteps)
+
+            # For DQN, check if the target network should be updated
+            # and update the exploration schedule
+            # For SAC/TD3, the update is dones as the same time as the gradient update
+            # see https://github.com/hill-a/stable-baselines/issues/900
+            self._on_step()
+
+            for idx, done in enumerate(dones):
+                if done:
+                    # Update stats
+                    num_collected_episodes += 1
+                    self._episode_num += 1
+
+                    if action_noise is not None:
+                        kwargs = dict(indices=[idx]) if env.num_envs > 1 else {}
+                        action_noise.reset(**kwargs)
+
+                    # Log training infos
+                    if log_interval is not None and self._episode_num % log_interval == 0:
+                        self.dump_logs()
+        callback.on_rollout_end()
+
+        return RolloutReturn(num_collected_steps * env.num_envs, num_collected_episodes, continue_training)
